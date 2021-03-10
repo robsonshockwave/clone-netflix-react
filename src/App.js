a0f1b32dcaf3b19c1ea67be67d84faee09204d14
Robson <robsonshockwavegithub@gmail.com>
2021-03-09 22:42:15 -0300

    Terminei o componente FeaturedMovie e Header finalizados

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
 import './App.css';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -27,8 +30,26 @@ export default () => {
     loadAll();
   }, []);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true)
+      } else {
+        setBlackHeader(false)
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, []);
+
   return (
     <div className="page">
+
+      <Header black={blackHeader} />
 
       { featuredData &&
         <FeaturedMovie item={featuredData} />
@@ -39,6 +60,12 @@ export default () => {
           <MovieRow key={key} title={item.title} items={item.items}/>
         ))}
       </section>
+      
+      <footer>
+        Feito com <span role="img" aria-label="coração">❤️</span> para estudos!<br/>
+        Direitos de imagem para Netflix<br/>
+        Dados pegos do site Themoviedb.org
+      </footer>
     </div>
   );
 }
